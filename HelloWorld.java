0b3a30c176b47aeec2c3b9abd188bd452cae8ec8
amolvishwarupe <amol_vishwarupe@yahoo.co.in>
2016-04-10 04:29:37 +0530

    JAVA Course Assignment #1

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package Common_task;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				
+		System.out.println("AMol Vishwarupe");
+
+	}
+
+}
---
+++ Session1_Assignments.java
@@ -0,0 +1,53 @@
+package Common_task;
+
+public class Session1_Assignments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int num1 = 10 ;		
+		float num2 = 20 ;		
+		int sum =0;		
+		
+		//Assignment2
+		int a,b=4,c ;
+		
+		
+		//Assignment3 assigning string to Integer
+		//a= "AMOL VISHWARUPE" ;
+		//Assigning float to integer
+		//num1 = 23.123456 ;
+		
+		
+		//Assignment 1		
+		sum = num1 + (int)num2 ;		
+		System.out.println("Sum = " + sum);
+		
+		
+		//Assignment 4
+		int x = 5 , y=10 , result ;
+		
+		result = x+y*2 ;
+		System.out.println("x+y*2 = " + result);
+		
+		result = x-y+2 ;
+		System.out.println("x-y+2 = " + result);
+		
+		result = (x+y)*2 ;
+		System.out.println("(x+y)*2 = " + result);
+		
+		result = y%x ;
+		System.out.println("y%x = " + result);
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
